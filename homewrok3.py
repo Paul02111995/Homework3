ba8109d48394df277d5d17ddaf6090c2f7948ab4
Paul02111995 <romanyk02111995@gmail.com>
2024-03-17 19:51:53 +0200

    task 4

--- homewrok3.py
+++ homewrok3.py
@@ -21,7 +21,7 @@ print (get_days_from_today("2021-10-09"))
 
 def get_numbers_ticket(min, max, quantity):
     if not (1 <= min <= max <= 1000) or not (1 <= quantity <= max - min + 1):
-        return []  # Исправлен отступ и добавлен пробел
+        return []
 
     numbers = random.sample(range(min, max + 1), quantity)
 
@@ -29,4 +29,8 @@ def get_numbers_ticket(min, max, quantity):
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print(f"Ваші лотерейні числа: {lottery_numbers}")
+
+
+# Задание 4
+
 
