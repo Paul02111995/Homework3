bbb0bd8d7456b4d7e7cfb55b813af6a33cc2117d
Paul02111995 <romanyk02111995@gmail.com>
2024-03-17 19:51:59 +0200

    Task 3

--- homewrok3.py
+++ homewrok3.py
@@ -1,5 +1,6 @@
-import random
-from datetime import datetime
+# import random
+from datetime import datetime, timedelta
+
 # Задание 1
 
 def get_days_from_today(date):
@@ -13,7 +14,7 @@ def get_days_from_today(date):
         return diff.days
     
     except ValueError:
-        return "Неверный формат даты. Используйте формат 'YYYY-MM-DD'."
+        return "Неверный формат даты."
     
 print (get_days_from_today("2021-10-09"))
 
@@ -32,5 +33,38 @@ print(f"Ваші лотерейні числа: {lottery_numbers}")
 
 
 # Задание 4
+from datetime import datetime, timedelta
 
+def get_upcoming_birthdays(users):
+    upcoming_birthdays = []
+    today = datetime.today().date()
 
+    for user in users:
+        birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        
+        birthday_this_year = birthday.replace(year=today.year)
+        if birthday_this_year < today:  
+            birthday_this_year = birthday.replace(year=today.year + 1)
+
+        days_to_birthday = (birthday_this_year - today).days
+
+        if 0 <= days_to_birthday <= 7:
+            congratulation_date = birthday_this_year
+
+            if congratulation_date.weekday() >= 5:
+                days_until_monday = (7 - congratulation_date.weekday()) % 7
+                congratulation_date += timedelta(days=days_until_monday)
+
+            upcoming_birthdays.append({
+                "name": user["name"],
+                "congratulation_date": congratulation_date.strftime("%Y.%m.%d"),
+            })
+
+    return upcoming_birthdays
+users = [
+    {"name": "John Doe", "birthday": "1985.03.19"},
+    {"name": "Jane Smith", "birthday": "1990.03.20"}
+]
+
+upcoming_birthdays = get_upcoming_birthdays(users)
+print("Список приветствий на этой неделе:", upcoming_birthdays)
